89ab4f2d4d9c5bf2ab14026167999d43307e612c
Irfan Ghat <58285388+projectfinalaudio@users.noreply.github.com>
2023-11-21 00:21:49 +0300

    Source files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use rocket::fs::{relative, FileServer};
 
+// Serve files manually
+mod manual {
+    use rocket::fs::NamedFile;
+    use std::path::{Path, PathBuf};
+
+    #[rocket::get("/second/<path..>")]
+    pub async fn other(path: PathBuf) -> Option<NamedFile> {
+        let mut path = Path::new(super::relative!("static")).join(path);
+        if path.is_dir() {
+            path.push("index.html");
+        }
+        NamedFile::open(path).await.ok()
+    }
+}
+
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from(relative!("static")))
+    rocket::build()
+        .mount("/", FileServer::from(relative!("static")))
+        .mount("/", rocket::routes![manual::other])
 }
